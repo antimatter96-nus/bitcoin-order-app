e338ad435587a378218a131cf1ce559248fc0f9b
Arpit Jain <me.arpitjain09@gmail.com>
2023-09-13 17:00:10 +0800

    (ci): fix incorrect project url

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker pull owasp/zap2docker-stable
-docker run -i owasp/zap2docker-stable zap-baseline.py -t "https://antinatter96-nus.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
+docker run -i owasp/zap2docker-stable zap-baseline.py -t "https://antimatter96-nus.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
